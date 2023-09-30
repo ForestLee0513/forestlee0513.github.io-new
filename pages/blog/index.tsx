70729f41ea1321138685526f86eed9e121ad08ad
ForestLee0513 <woolimlee.dev@gmail.com>
2023-10-01 00:15:22 +0900

    [update] Apply list styles in blog article list

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -5,9 +5,8 @@ import { useTranslation } from "next-i18next";
 
 import { getAllLocaledArticles } from "~/lib/markdownParser";
 import Post from "~/types/article";
-import { AuthorCard, List } from "~/components/article";
-import bio from "~/bio";
 import HeadMeta from "~/components/HeadMeta";
+import Image from "next/image";
 
 type Props = {
   articles: Post[];
@@ -22,16 +21,30 @@ const Blog = ({ articles }: Props) => {
       <div className="col-span-4 pc:col-span-2">
         <form>
           <input type="text" className="w-full" />
-          <h2 className="hidden pc:block">Category</h2>
+          <h2 className="hidden pc:block mt-[40px]">Category</h2>
+          {/* Category list */}
+          <ul className="m-0 mt-[10px] p-0 list-none flex overflow-x-auto pc:block">
+            <li className="m-0 first:pl-0 last:pr-0 p-[10px] first:pc:pl-[10px] last:pc:pr-[10px] block pc:list-item">
+              sample Category 1
+            </li>
+            <li className="m-0 first:pl-0 last:pr-0 p-[10px] first:pc:pl-[10px] last:pc:pr-[10px] block pc:list-item">
+              sample Category 2
+            </li>
+          </ul>
         </form>
       </div>
-      <ul className="w-full grid pc:grid-cols-3 m-0 p-0 pc:gap-x-[40px] col-span-4 pc:col-span-6">
-        <li className="col-span-1 m-0 p-0"></li>
-        <li className="col-span-1 m-0 p-0"></li>
-        <li className="col-span-1 m-0 p-0"></li>
-        <li className="col-span-1 m-0 p-0"></li>
-        <li className="col-span-1 m-0 p-0"></li>
-        <li className="col-span-1 m-0 p-0"></li>
+      {/* Articles */}
+      <ul className="w-full grid pc:grid-cols-3 m-0 p-0 pc:gap-[40px] gap-y-[20px] pt-[20px] pc:pt-0 col-span-4 pc:col-span-6">
+        <li className="col-span-1 m-0 p-0 flex flex-col">
+          <div className="w-full max-h-[20vw] bg-secondary"></div>
+          <div className="flex flex-col mt-[20px]">
+            <h3>Test</h3>
+            <p className="mt-[10px] mb-0">2023.01.01</p>
+            <p className="mt-[10px] mb-0">
+              The quick brown fox jumps over the lazy dog
+            </p>
+          </div>
+        </li>
       </ul>
 
       {/* <List
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -27,21 +27,27 @@ module.exports = {
             fontFamily: "Pretendard, sans-serif",
             h1: {
               fontSize: "3rem",
+              margin: 0,
             },
             h2: {
               fontSize: "2.5rem",
+              margin: 0,
             },
             h3: {
               fontSize: "2.062rem",
+              margin: 0,
             },
             h4: {
               fontSize: "1.75rem",
+              margin: 0,
             },
             h5: {
               fontSize: "1.438rem",
+              margin: 0,
             },
             h6: {
               fontSize: "1.188rem",
+              margin: 0,
             },
             a: {
               margin: 0,
