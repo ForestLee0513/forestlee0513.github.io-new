d9a8153170296151a7fe1e2533c592418edb4e6d
ForestLee0513 <woolimlee.dev@gmail.com>
2023-10-01 14:16:38 +0900

    [fix] Apply scroll in mobile blog page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,20 +1,17 @@
 import { GetStaticPropsContext } from "next";
 
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { useTranslation } from "next-i18next";
 
 import { getAllLocaledArticles } from "~/lib/markdownParser";
 import Post from "~/types/article";
 import HeadMeta from "~/components/HeadMeta";
-import Image from "next/image";
+import { useEffect } from "react";
 
 type Props = {
   articles: Post[];
 };
 
 const Blog = ({ articles }: Props) => {
-  const { t } = useTranslation("common");
-
   return (
     <>
       <HeadMeta useDyanmicThumbnail={false} />
@@ -24,10 +21,10 @@ const Blog = ({ articles }: Props) => {
           <h2 className="hidden pc:block mt-[40px]">Category</h2>
           {/* Category list */}
           <ul className="m-0 mt-[10px] p-0 list-none flex overflow-x-auto pc:block">
-            <li className="m-0 first:pl-0 last:pr-0 p-[10px] first:pc:pl-[10px] last:pc:pr-[10px] block pc:list-item">
+            <li className="m-0 first:pl-0 last:pr-0 p-[10px] first:pc:pl-[10px] last:pc:pr-[10px] block pc:list-item grow-0 shrink-0 basis-auto">
               sample Category 1
             </li>
-            <li className="m-0 first:pl-0 last:pr-0 p-[10px] first:pc:pl-[10px] last:pc:pr-[10px] block pc:list-item">
+            <li className="m-0 first:pl-0 last:pr-0 p-[10px] first:pc:pl-[10px] last:pc:pr-[10px] block pc:list-item grow-0 shrink-0 basis-auto">
               sample Category 2
             </li>
           </ul>
@@ -36,7 +33,29 @@ const Blog = ({ articles }: Props) => {
       {/* Articles */}
       <ul className="w-full grid pc:grid-cols-3 m-0 p-0 pc:gap-[40px] gap-y-[20px] pt-[20px] pc:pt-0 col-span-4 pc:col-span-6">
         <li className="col-span-1 m-0 p-0 flex flex-col">
-          <div className="w-full max-h-[20vw] bg-secondary"></div>
+          <div className="w-full max-h-[60vh] pc:h-[30vh] bg-secondary m-0">
+            <img
+              src="/assets/images/author.png"
+              alt="thumbnail"
+              className="w-full h-full m-0"
+            />
+          </div>
+          <div className="flex flex-col mt-[20px]">
+            <h3>Test</h3>
+            <p className="mt-[10px] mb-0">2023.01.01</p>
+            <p className="mt-[10px] mb-0">
+              The quick brown fox jumps over the lazy dog
+            </p>
+          </div>
+        </li>
+        <li className="col-span-1 m-0 p-0 flex flex-col">
+          <div className="w-full max-h-[60vh] pc:h-[30vh] bg-secondary m-0">
+            <img
+              src="/assets/images/default-thumbnail.png"
+              alt="thumbnail"
+              className="w-full h-full m-0"
+            />
+          </div>
           <div className="flex flex-col mt-[20px]">
             <h3>Test</h3>
             <p className="mt-[10px] mb-0">2023.01.01</p>
