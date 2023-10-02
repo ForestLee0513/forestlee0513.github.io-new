72cf4f09b3873bba8c598ce0fa0f1a5d9e198165
ForestLee0513 <woolimlee.dev@gmail.com>
2023-10-02 16:58:51 +0900

    [chore] Remove unused components

--- components/article/index.tsx
+++ components/article/index.tsx
@@ -1,6 +1,5 @@
 import AuthorCard from "./AuthorCard";
 import Body from "./Body";
 import Header from "./Header";
-import List from "./List";
 
-export { AuthorCard, Body, Header, List };
+export { AuthorCard, Body, Header };
