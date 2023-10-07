2ca504e43b4002d73900532e6f0b3439663f9c84
ForestLee0513 <woolimlee.dev@gmail.com>
2023-10-07 16:15:11 +0900

    [feat] Add textarea input component

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,20 +1,56 @@
 import { InputHTMLAttributes } from "react";
+import { twMerge } from "tailwind-merge";
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+interface InputProps
+  extends InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement> {
   leftChildren?: JSX.Element;
   rightChildren?: JSX.Element;
+  containerClassName?: string; // className of container div element.
+  inputClassName?: string; // className of real input.
+  resize?: boolean;
+  rows?: number;
 }
 
 const Input = ({
   leftChildren,
   rightChildren,
-  className,
+  containerClassName = "",
+  inputClassName = "",
+  resize,
   ...restProps
 }: InputProps) => {
+  if (resize) {
+    return (
+      <div
+        className={twMerge(
+          `py-[6px] pc:py-[10px] border-b-[2px] flex ${containerClassName}`
+        )}
+      >
+        {leftChildren}
+        <textarea
+          className={twMerge(
+            `w-full bg-[transparent] [&:nth-child(n+2)]:pl-[10px] ${inputClassName} resize-y`
+          )}
+          {...restProps}
+        />
+        {rightChildren}
+      </div>
+    );
+  }
+
   return (
-    <div className="p-[6px] pc:p-[10px] border-b-[2px] flex">
+    <div
+      className={twMerge(
+        `py-[6px] pc:py-[10px] border-b-[2px] flex ${containerClassName}`
+      )}
+    >
       {leftChildren}
-      <input className="w-full bg-[transparent] pl-[10px]" {...restProps} />
+      <input
+        className={twMerge(
+          `w-full bg-[transparent] [&:nth-child(n+2)]:pl-[10px] ${inputClassName}`
+        )}
+        {...restProps}
+      />
       {rightChildren}
     </div>
   );
