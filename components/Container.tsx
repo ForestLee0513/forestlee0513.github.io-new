2b9afb3ebea9df78df54f464bf059095da810e3c
ForestLee0513 <woolimlee.dev@gmail.com>
2023-10-16 21:06:40 +0900

    [fix] Set max width for wide screen resolution

--- components/Container.tsx
+++ components/Container.tsx
@@ -7,7 +7,7 @@ const Container = ({
 }: HTMLAttributes<HTMLDivElement>) => {
   return (
     <div
-      className={`grid grid-cols-4 pc:grid-cols-8 pc:gap-[40px] mx-auto max-w-[90%] prose dark:prose-invert auto-rows-max w-full ${
+      className={`grid grid-cols-4 pc:grid-cols-8 pc:gap-[40px] mx-auto max-w-[90%] pc:max-w-[1920px] prose dark:prose-invert auto-rows-max w-full ${
         className || ""
       }`}
       {...rest}
