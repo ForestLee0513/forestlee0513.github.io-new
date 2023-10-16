e9c51df3f2e8398b52175dba1d8a41787a1ebcfa
ForestLee0513 <woolimlee.dev@gmail.com>
2023-10-16 13:51:07 +0900

    [update] Change thickness of underline

--- components/nav/NavItem.tsx
+++ components/nav/NavItem.tsx
@@ -9,12 +9,14 @@ interface INavItem extends HTMLAttributes<HTMLDivElement> {
 const NavItem = ({ children, className, href, ...rest }: INavItem) => {
   const { pathname } = useRouter();
   const isSamePath = pathname === href;
-  const activeStyle = isSamePath ? "font-bold underline" : "";
+  const activeStyle = isSamePath
+    ? "font-black underline underline-offset-2 decoration-2"
+    : "";
 
   return (
     <Link href={href}>
       <p
-        className={`z-[1] bg-background flex justify-start pc:justify-end m-0 py-[20px] pc:py-0 pc:last:pr-0 last:border-b-2 last:pc:border-b-0 px-[5%] pc:px-0 pc:mx-[10px] cursor-pointer ${activeStyle} ${
+        className={`z-[1] bg-background flex justify-start pc:justify-end m-0 py-[20px] pc:py-0 pc:last:pr-0 last:border-b-2 last:pc:border-b-0 px-[5%] pc:px-0 pc:mx-[10px] cursor-pointer text-text ${activeStyle} ${
           className || ""
         }`}
         {...rest}
